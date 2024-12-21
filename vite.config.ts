8591366aaad05671ee0e2ad49e02624ac1723717
arkady <arkadychakhalyan@gmail.com>
2024-12-21 19:58:18 +0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    registerType: 'prompt',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+        name: 'CashbackCheck',
+        short_name: 'CashbackCheck',
+        description: 'Track your cashbacks across all your banks',
+        icons: [
+            {
+                src: '/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: '/apple-touch-icon.png',
+                sizes: '180x180',
+                type: 'image/png',
+                purpose: 'apple touch icon',
+            },
+            {
+                src: '/maskable_icon.png',
+                sizes: '225x225',
+                type: 'image/png',
+                purpose: 'any maskable',
+            },
+        ],
+        theme_color: '#ee857f',
+        background_color: '#120b1b',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+
+    }
+}
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA(manifestForPlugin)],
     server: {
         port: 3000,
         open: true,
