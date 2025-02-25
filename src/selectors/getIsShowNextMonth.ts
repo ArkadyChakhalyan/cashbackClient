1bb547dea87ba1d8b2b782e5b434db18982aa324
arkady <arkadychakhalyan@gmail.com>
2025-02-25 19:06:38 +0300

    fix next month

--- src/selectors/getIsShowNextMonth.ts
+++ src/selectors/getIsShowNextMonth.ts
@@ -2,5 +2,5 @@ import { SHOW_NEXT_MONTH_DAY } from '../constants.ts';
 
 export const getIsShowNextMonth = (): boolean => {
     const date = new Date();
-    return date.getDay() > SHOW_NEXT_MONTH_DAY;
+    return date.getDate() >= SHOW_NEXT_MONTH_DAY;
 };
