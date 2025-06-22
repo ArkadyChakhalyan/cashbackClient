2eabd9dc2751ad9b6da5f00a5a08610dde30a077
arkady <arkadychakhalyan@gmail.com>
2025-06-22 16:32:20 +0300

    release-1.2.2

--- src/selectors/getCashbackCodesInfo.ts
+++ src/selectors/getCashbackCodesInfo.ts
@@ -3,9 +3,6 @@ import { EBank, ICashback } from 'cashback-check-types';
 import { IBankCashbackCodeInfo, TBankCashbackCodes, TBankCashbackLoyaltyProgram } from '../types.ts';
 
 const isBankCashbackCodes = (item: TBankCashbackLoyaltyProgram): item is TBankCashbackCodes => {
-    if (item === null || item === undefined) {
-        return false;
-    }
     return !Array.isArray(item) && Object.keys(item).every(key => {
         const value = item[key];
         return value && typeof value === 'object' && 'codes' in value && Array.isArray(value.codes);
@@ -16,9 +13,11 @@ export const getCashbackCodesInfo = (
     bank: EBank,
     name: string,
 ): IBankCashbackCodeInfo[] => {
+    return [] as IBankCashbackCodeInfo[];
     const codeInfos: IBankCashbackCodeInfo[] = [];
     const bankCodes = CASHBACKS_CODES[bank];
     if (isBankCashbackCodes(bankCodes) && bankCodes && bankCodes[name]) {
+        //@ts-ignore
         codeInfos.push(bankCodes[name]);
     } else {
         for (let loyaltyProgram in bankCodes as {[key: string]: TBankCashbackCodes}) {
