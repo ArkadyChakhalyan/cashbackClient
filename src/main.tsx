f63c51e223560bcc230a2bb25f3b6a4f1f3dd721
arkady <arkadychakhalyan@gmail.com>
2026-02-18 19:47:00 +0300

    release-1.2.9

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import store from './store/store.ts';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './auth/authContext.tsx';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './style/theme.ts';
